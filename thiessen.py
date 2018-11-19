71d48f3e9fb72eec3a45a1febee0b87b3a447db8
Carlos <merac1999@gmail.com>
2018-11-19 10:27:19 -0500

    Eliminando comentarios

--- thiessen.py
+++ thiessen.py
@@ -10,7 +10,7 @@ def build_graph(tree, G):
     G[tree.getAuthor()] = {}
   ocurrencias = []
   for subcomment in tree.getComments():
-    # assert tree.getDate() <= subcomment.getDate()
+    assert tree.getDate() <= subcomment.getDate()
     delta = get_seconds(tree.getDate(), subcomment.getDate())
     aux = build_graph(subcomment, G)
     aux.append([subcomment.getAuthor(), 0])
@@ -55,37 +55,12 @@ def thiessen(tree):
   centros = {}
   distance = {}
   usuario = {}
-  # distance1 = {}
-  # distance2 = {}
-  # print(len(G))
   k = k if k > 2 else 2
   for i in range(len(participacion)):
     if i < k:
       centros[participacion[i][0]] = set()
     distance[participacion[i][0]] = INF
     usuario[participacion[i][0]] = ""
-  #   distance1[participacion[i][0]] = INF
-  #   distance2[participacion[i][0]] = INF
-
-  # heap1 = [("derawin07", 0)]
-  # heap2 = [("None", 0)]
-  # while len(heap1):
-  #   u, wi = heappop(heap1)
-  #   for v in G[u]:
-  #     acumulado, n = G[u][v]
-  #     promedio = acumulado//n
-  #     if wi + promedio < distance1[v]:
-  #       distance1[v] = wi + promedio
-  #       heappush(heap1, (v, wi + promedio))
-  # while len(heap2):
-  #   u, wi = heappop(heap2)
-  #   for v in G[u]:
-  #     acumulado, n = G[u][v]
-  #     promedio = acumulado//n
-  #     if wi + promedio < distance2[v]:
-  #       distance2[v] = wi + promedio
-  #       heappush(heap2, (v, wi + promedio))
-  # print(distance1["bargainhunterrr17"], distance2["bargainhunterrr17"])
   
   for source in centros:
     heap = [(source, 0)]
@@ -110,11 +85,7 @@ def thiessen(tree):
               if v in centros[last] and last > source:
                 centros[last].remove(v)
                 centros[source].add(v)
-    # last = source
-  # for i in G:
-  #   print(i)
-  #   print(G[i])
-  # print(distance1["_Person_"], distance2["_Person_"])
+
   for centro in centros:
     print(centro, end=" ")
     print(*centros[centro], end=" ")
